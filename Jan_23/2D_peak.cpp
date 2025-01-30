92fb870e3a842a5a0609e62a18ac5e85eccaa078
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2025-01-30 08:33:53 +0530

    Program for finding the 2D peak through iteration

--- Jan_23/2D_peak.cpp
+++ Jan_23/2D_peak.cpp
@@ -7,24 +7,13 @@ void two_d_peak(int arr[][3], int n){
         for (int j = 0 ;j < n ; j ++)
         {
             if ((arr[i][j] > arr[i-1][j] )&&(arr[i][j] > arr[i+1][j] )&&(arr[i][j] > arr[i][j-1] )&&(arr[i][j] > arr[i][j+1] )){
-            cout<<"Element is the peak element "<<arr[i][j]<<endl;
+            cout<<"Peak element is "<<arr[i][j]<<" at ("<<i<<","<<j<<")"<<endl;
             break;
             }
         }
     }
 }
 int main(){
-    // int a[3][3]={{2,5,8},{5,3,9},{1,4,7}}
-    int n ;
-    cout<<"Enter the number"<<endl;
-    cin>>n ;
-    int arr[3][3];
-    // int val;
-    for (int i = 0;i <n ; i++){
-        for(int j = 0;j < n;j++)
-        {
-            cin>>arr[i][j] ;
-        }
-    }
-    two_d_peak(arr,n);
+    int arr[3][3]={{2,5,8},{5,3,9},{1,4,7}};
+    two_d_peak(arr,3);
 }
