da7413e3fb721475e846e523212f3bba89d4b766
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2025-02-06 00:43:29 +0530

    Insertion Sort

---
+++ Jan_30/Insertion-Sort/insertion.cpp
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+void insertion_sort(int *a, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = a[i];
+        int j = i - 1;
+
+        while (j >= 0 && a[j] > temp)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = temp;
+    }
+}
+
+int main()
+{
+
+    int n = 10;
+    int m = 50;
+
+    FILE *fp = fopen("insertionData.csv", "w");
+    if (fp == NULL)
+    {
+        printf("Could not open file %s\n", "insertionData.csv");
+    }
+    fprintf(fp, "Size , Time(ns)\n");
+
+    for (int i = 0; i < 1000; i++)
+    {
+        int *a = new int[n];
+        for (int j = 0; j < n; j++)
+        {
+            a[j] = rand() % m;
+        }
+
+        auto start = high_resolution_clock::now();
+        insertion_sort(a, n);
+        auto end = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(end - start);
+
+        fprintf(fp, "%d,%lld\n", n, duration.count());
+        n += 100;
+        m += 100;
+    }
+    fclose(fp);
+
+    cout << "Data successfully uploaded" << endl;
+
+    return 0;
+}
