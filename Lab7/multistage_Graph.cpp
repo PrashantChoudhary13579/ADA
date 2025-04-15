110a5d22be0386364bb2eb320959297a6057c5a8
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2025-04-15 19:20:15 +0530

    Multistage graph

--- Lab7/multistage_Graph.cpp
+++ Lab7/multistage_Graph.cpp
@@ -1,35 +1,39 @@
-#include<iostream>
-#include<vector>
-#include<climits>
+#include <iostream>
+#include <vector>
+#include <climits>
 using namespace std;
 
 #define N 12
 #define INF INT_MAX
 
-void ShortestDistance(int graph[N][N]){
+void ShortestDistance(int graph[N][N]) {
+    vector<int> dist(N, INF);
+    vector<int> next(N, -1);  
+    dist[N - 1] = 0; 
 
-    vector<int> dist(N,INF);
-    dist[N-1] = 0;
-    cout<<N-1<<" -> "<<dist[N-1]<<endl;
-    vector<int> d(N,-1);
-    int val;
-    for(int i = N-2; i>=0 ; i--){    
-        for( int j = i+1 ; j < N ; j++){
-            if(graph[i][j] != INF && graph[i][j] + dist[j] < dist[i])
-            {
+    for (int i = N - 2; i >= 0; i--) {
+        for (int j = i + 1; j < N; j++) {
+            if (graph[i][j] != INF && graph[i][j] + dist[j] < dist[i]) {
                 dist[i] = graph[i][j] + dist[j];
-                d.push_back(j);
+                next[i] = j;  
             }
-
         }
     }
 
-    cout<<"Minimum distance from source to sink = "<<dist[0]<<endl;
+    cout << "\nMinimum distance from source (0) to sink (" << N - 1 << ") = " << dist[0] << endl;
+
+    cout << "Path followed: ";
+    int current = 0;
+    while (current != -1) {
+        cout << current;
+        if (next[current] != -1) cout << " -> ";
+        current = next[current];
+    }
+    cout << endl;
 }
 
-int main(){
-    
-    int grap[12][12]={
+int main() {
+    int grap[12][12] = {
         {INF, 7 , 3 , 1 , 4 ,INF,INF,INF,INF,INF,INF,INF},
         {INF,INF,INF,INF,INF, 8 , 4 ,INF,INF,INF,INF,INF},
         {INF,INF,INF,INF,INF, 6 , 10,INF,INF,INF,INF,INF},
@@ -43,18 +47,7 @@ int main(){
         {INF,INF,INF,INF,INF,INF,INF,INF,INF,INF,INF, 9 },
         {INF,INF,INF,INF,INF,INF,INF,INF,INF,INF,INF,INF}
     };
+
     ShortestDistance(grap);
     return 0;
 }
-
-
-// int graph[N][N] =
-    // {{INF, 1, 2, 5, INF, INF, INF, INF},
-    //  {INF, INF, INF, INF, 4, 11, INF, INF},
-    //  {INF, INF, INF, INF, 9, 5, 16, INF},
-    //  {INF, INF, INF, INF, INF, INF, 2, INF},
-    //  {INF, INF, INF, INF, INF, INF, INF, 18},
-    //  {INF, INF, INF, INF, INF, INF, INF, 13},
-    //  {INF, INF, INF, INF, INF, INF, INF, 2},
-    // {INF, INF, INF, INF, INF, INF, INF, INF}};
- 
